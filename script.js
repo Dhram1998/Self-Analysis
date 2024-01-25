3829af185f7776085f4720c66e0c594d874f6dc2
Dharmendra Jha <rahuljhabanka@gmail.com>
2024-01-25 20:40:33 +0530

    form validation sitll in prograses

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const changeText = function () {
 document.querySelector("span").addEventListener("click", function () {
   changeText();
 });
+
+
+/*************************************** Form Validation *************************************************/
+
+function seterror(id, error){
+  // sets error inside tag if id
+
+  element = document.getElementById(id);
+  element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm(){
+  var returnval = true;
+
+  // perform validation and if validation fails, set the value of returnval of false
+  var name = document.forms['myForm']["fname"].value;
+ 
+  if(name.length < 5){
+    seterror("name", "*too short");
+    returnval = false;
+  }
+
+
+  return returnval;
+}
